c6c5e2df32b6550b635a2d500e75a39412e25572
assis-souz4 <assissouza-dev@hotmail.com>
2024-02-25 14:08:23 -0300

    first commit

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,4 +1,4 @@
-import 'package:cli/cli.dart' as cli;
+// import 'package:cli/cli.dart' as cli;
 
 class Person {
   String firstName;
@@ -6,6 +6,11 @@ class Person {
   int? age;
 
   Person(this.firstName, this.lastName, this.age);
+
+//3º questão
+  bool checkPerson() {
+    return lastName.toLowerCase() == 'souza';
+  }
 }
 
 void main(List<String> arguments) {
@@ -13,17 +18,38 @@ void main(List<String> arguments) {
     Person('Clerton', 'Leal', 38),
     Person('Assis', 'Souza', 38),
     Person('Juliana', 'Souza', 28),
-    Person('Martim', 'Matus', 4),
+    Person('Martim', 'Marques', 4),
     Person('Edilange', 'Souza', 65),
   ];
 
   // 1 - Filter the people with last name iquals Assis
 
-  // 2 - Filter the people less than 25 years
+  var souzaLastName =
+      people.where((element) => element.lastName.toLowerCase() == 'souza');
 
-  // 3 - Create a method that receive a Person as parameter and 
-  // return true or false if the person have last name 
+  souzaLastName.forEach((person) {
+    print('${person.firstName} ${person.lastName}');
+  });
+  print('-' * 20);
+
+  // 2 - Filter the people less than 25 years
+  var ageUnder25 = people.where((element) => element.age! < 25);
+
+  ageUnder25.forEach((element) {
+    print('${element.firstName} ${element.lastName}');
+  });
+  print('-' * 20);
+
+  // 3 - Create a method that receive a Person as parameter and
+  // return true or false if the person have last name
   // equals Souza
+  for (var person in people) {
+    print('${person.firstName} ${person.lastName} => ${person.checkPerson()}');
+  }
+  print('-' * 20);
 
   // 4 - Print all persons on people list
+  for(var person in people){
+    print('Name: ${person.firstName} ${person.lastName}, Age: ${person.age}');
+  }
 }
